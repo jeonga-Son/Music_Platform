fa5a67316f5b78de1d269b184e3ea60a1896423b
jeonga <jjeong0106@gmail.com>
2022-10-11 15:43:08 +0900

    유틸, initData 기반, JPA 기반, AppConfg 기반 코드

--- src/main/java/com/ll/exam/app__2022_10_11/App20221011Application.java
+++ src/main/java/com/ll/exam/app__2022_10_11/App20221011Application.java
@@ -2,12 +2,24 @@ package com.ll.exam.app__2022_10_11;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+// @EnableJpaAuditing은 Entity를 기획할 때 모든 엔티티에 들어가는 공통 컬럼들이 있는데 (ex. 등록일자, 수정일자 등) 공통 Entity를 뽑아내서 사용하고자 할 때 사용한다.
+@EnableJpaAuditing
 public class App20221011Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(App20221011Application.class, args);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		// BCryptPasswordEncoder는 BCrypt 해싱 함수(BCrypt hashing function)를 사용해서 비밀번호를 인코딩해주는 메서드와 사용자의 의해 제출된 비밀번호와 저장소에 저장되어 있는 비밀번호의 일치 여부를 확인해주는 메서드를 제공한다ㄴ.
+		return new BCryptPasswordEncoder();
 	}
 
 }
---
+++ src/main/java/com/ll/exam/app__2022_10_11/app/base/entity/BaseEntity.java
@@ -0,0 +1,55 @@
+package com.ll.exam.app__2022_10_11.app.base.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@SuperBuilder
+// 공통 매핑 정보가 필요할 때, 부모 클래스에 선언하고 속성만 상속 받아서 사용하고 싶을 때 @MappedSuperclass를 사용한다.
+@MappedSuperclass
+@NoArgsConstructor(access = PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@ToString
+public class BaseEntity {
+
+    @Id
+    // @GeneratedValue(strategy = IDENTITY)는 기본 키 생성을 데이터베이스에 위임한다.
+    // 즉, id 값을 null로 하면 DB가 알아서 AUTO_INCREMENT 해준다.
+    @GeneratedValue(strategy = IDENTITY)
+    private long id;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
+
+    @Transient // 아래 필드가 DB 필드가 되는 것을 막는다.
+    @Builder.Default
+    private Map<String, Object> extra = new LinkedHashMap<>();
+
+
+    public BaseEntity(long id) {
+        this.id = id;
+    }
+
+    public BaseEntity(LocalDateTime createDate, LocalDateTime modifyDate) {
+        this.createDate = createDate;
+        this.modifyDate = modifyDate;
+    }
+}
