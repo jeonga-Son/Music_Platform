c6270f523f61d0ea32efed37b654356131e9ce4e
jeonga <jjeong0106@gmail.com>
2022-10-14 11:12:02 +0900

    RebateOrderItem 목록 페이지 추가

--- src/main/java/com/ll/exam/app__2022_10_11/app/rebate/controller/AdmRebateController.java
+++ src/main/java/com/ll/exam/app__2022_10_11/app/rebate/controller/AdmRebateController.java
@@ -1,15 +1,14 @@
 package com.ll.exam.app__2022_10_11.app.rebate.controller;
 
 import com.ll.exam.app__2022_10_11.app.rebate.service.RebateService;
+import com.ll.exam.app__2022_10_11.util.Ut;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/adm/rebate")
@@ -26,10 +25,19 @@ public class AdmRebateController {
 
     @PostMapping("/makeData")
     @PreAuthorize("hasAuthority('ADMIN')")
-    @ResponseBody
     public String makeData(String yearMonth) {
         rebateService.makeDate(yearMonth);
 
-        return "성공";
+        return "redirect:/adm/rebate/rebateOrderItemList?yearMonth=" + yearMonth + "&msg=" + Ut.url.encode("정산데이터가 성공적으로 생성되었습니다.");
+    }
+
+    @GetMapping("/rebateOrderItemList")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public String showRebateOrderItemList(String yearMonth) {
+        if (yearMonth == null) {
+            yearMonth = "2022-10";
+        }
+
+        return "adm/rebate/rebateOrderItemList";
     }
 }
